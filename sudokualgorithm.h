ae9724694265b7f8032a16e6747209e108224b66
Vitaliy Datsiuk <vetalvetal222@gmail.com>
2019-12-16 10:14:17 +0200

    Update sudokualgorithm
    Added functions that check:
    when the game is over
    when there is a conflict

--- sudokualgorithm.h
+++ sudokualgorithm.h
@@ -13,11 +13,29 @@ public:
     SudokuBoard* board() const;
     void createMap();
     void createQuestion(int numbersOfEmpty=-1);
+    QVector<int> checkCell(int row, int column, QVector<int>);
 
+signals:
+    void winler(const int signal);
+    void setNewBoard();
+
+public slots:
+    void pressed(const QString &input);
+    void checkChange();
+    void resetGame(const int state);
+    void customCheck();
+    void solveTheMap();
 
 
 protected:
+    void setupBoard(int level=0);
     bool keepMoving(int index, QVector<int> map);
+    int randomChoose(QVector<int> &avNum);
+    void checkWin();
+    void checkConflict();
+    void customInput();
+    bool solvable();
+    bool solve(int index, QVector<int> map);
 
 private:
     SudokuBoard* b_board=nullptr;
